c876e13d9225e6a6a72713a2809b446b9b5180cb
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-11-21 18:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the body element
+    var body = document.body;
+
+    // Check if the body has any child nodes
+    if (body.hasChildNodes()) {
+      // If it has child nodes, remove them
+      while (body.firstChild) {
+        body.removeChild(body.firstChild);
+      }
+    }
+
+    // Create a text node with the content "DOM load success"
+    var textNode = document.createTextNode('DOM load success');
+
+    // Append the text node to the body
+    body.appendChild(textNode);
+  });
